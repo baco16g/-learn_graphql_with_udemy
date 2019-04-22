78d953d5acb002fb23626d2662332d04e05847d5
baco16g <baco.16g@gmail.com>
2019-04-22 16:07:29 +0900

    feat: add a secret setting, and add a password field to User model

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'localhost:4466'
+  endpoint: 'localhost:4466',
+  secret: 'thisismysupersecrettext'
 })
 
 export { prisma as default }
