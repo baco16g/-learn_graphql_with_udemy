f6fd6d37880e1ece554d1fb87b646eb763aa6029
baco16g <baco.16g@gmail.com>
2019-05-14 18:21:09 +0900

    feat: add test cases for subscription

--- graphql-prisma/__tests__/comments.test.js
+++ graphql-prisma/__tests__/comments.test.js
@@ -2,7 +2,11 @@
 
 import 'cross-fetch/polyfill'
 import prisma from '../src/prisma'
-import seedDatabase, { comments, users } from '../jest/utils/seedDatabase'
+import seedDatabase, {
+  comments,
+  users,
+  posts
+} from '../jest/utils/seedDatabase'
 import getClient from '../jest/utils/getClient'
 import * as operations from '../jest/utils/operations'
 
@@ -24,4 +28,29 @@ test('Should not delete other users comment', async () => {
   await expect(
     client.mutate({ mutation: operations.deleteComment, variables })
   ).rejects.toThrow()
+})
+
+test('Should subscribe to comments for a post', async done => {
+  const client = getClient(users[1].jwt)
+  const variables = { postId: posts[0].post.id }
+  client
+    .subscribe({ query: operations.subscribeToComments, variables })
+    .subscribe({
+      next(response) {
+        expect(response.data.comment.mutation).toBe('DELETED')
+        done()
+      }
+    })
+  await prisma.mutation.deleteComment({ where: { id: comments[0].comment.id } })
+})
+
+test('Should subscribe to changes for published posts', async done => {
+  const client = getClient(users[0].jwt)
+  client.subscribe({ query: operations.subscribeToPosts }).subscribe({
+    next(response) {
+      expect(response.data.post.mutation).toBe('DELETED')
+      done()
+    }
+  })
+  await prisma.mutation.deletePost({ where: { id: posts[0].post.id } })
 })
